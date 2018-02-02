e8307020665ec3103cf5716285259d62e68ed951
Daniele Tommasini <daniele.tommasini.dt@gmail.com>
2018-02-02 15:52:05 +0000

    running in the 90s

--- index.php
+++ index.php
@@ -1,5 +1,32 @@
 <html>
+    <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link href="style.css" rel="stylesheet" type="text/css">
+  
+  	
+  </head>
     <body>
-        <h1>Il nostro favoloso sito</h1>
+  
+        <h1><p style="color : green">A</p><p>rduino</p><p style="color:red">F</p><p>low</p><p style="color:blue">Stack</p></h1>
+        
+        
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">AFS</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+  
     </body>
 </html>
