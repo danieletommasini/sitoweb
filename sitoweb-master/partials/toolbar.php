69c6795f9d5a24cd90a473dc0c12aba44189bb55
Daniele Tommasini <daniele.tommasini.dt@gmail.com>
2018-03-11 17:07:23 +0000

    fix login

--- sitoweb-master/partials/toolbar.php
+++ sitoweb-master/partials/toolbar.php
@@ -35,7 +35,22 @@
             <?php
     session_start();
     if(!isset($_SESSION["id"])){
-      include "login.php";
+     echo "<form class='form' action='loginregister.php' method='post'> <br>
+<div class='input-group'>
+      <span class='input-group-addon'><i class='glyphicon glyphicon-user'></i></span>
+      
+      <input id='username' type='text' class='form-control' name='username' placeholder='username'<!--INPUT-->
+    </div>
+    <div class='input-group'>
+      <span class='input-group-addon'><i class='glyphicon glyphicon-lock'></i></span>
+      
+      <input id='password' type='password' class='form-control' name='password' placeholder='Password'><!--INPUT-->
+    </div><br>
+		<input class='btn btn-primary btn-md' type='submit' name='register' value='SIGN IN' style='padding: 5px 16px; border-radius:5px;' > 
+		<input class='btn btn-primary btn-md' type='submit' name='login' value='LOG IN' style='padding: 5px 16px; border-radius:5px;' >
+</form>
+
+";
     } else {
       echo $_SESSION["id"] . " <form class='form' action='loginregister.php' method='post'>
             <input type='submit' name='logout' value='LOGOUT'><br><br> 
