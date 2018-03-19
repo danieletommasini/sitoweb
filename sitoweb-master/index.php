3cc4467af0f3c606e722502941b914ff8594ad4b
Daniele Tommasini <daniele.tommasini.dt@gmail.com>
2018-03-19 16:06:51 +0000

    try index 1.0v

--- sitoweb-master/index.php
+++ sitoweb-master/index.php
@@ -8,5 +8,61 @@
 </div>-->
 <?php require("partials/toolbar.php");?>
 <body>
+
+   <div class="col-sm-6" style="background-color:lavender; border-radius: 2%;z-index:-3" >
+  <h2>First example</h2>
+  <p>Problem with helloword</p><br><br>
+  <p>
+#include <cstdlib>
+#include <iostream>
+ <br>
+void NUMEROPRIMO (int A);
+ <br>
+ 
+using namespace std;
+ <br>
+int main()
+{{
+ <br>
+ int A;<br>  
+   cin>>A; <br>
+NUMEROPRIMO(A);<br>
+}
+    system("PAUSE");<br>
+    return EXIT_SUCCESS;<br>
+}
+  
+  <!-- Left-aligned media object -->
+  <div class="media">
+    <div class="media-left">
+      <img src="img_avatar1.png" class="media-object" style="width:60px">
+    </div>
+    <div class="media-body">
+      <h4 class="media-heading">Left-aligned</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div>
+  <hr>
+  
+  <!-- Right-aligned media object -->
+  <div class="media">
+    <div class="media-body">
+      <h4 class="media-heading">Right-aligned</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+    <div class="media-right">
+      <img src="img_avatar1.png" class="media-object" style="width:60px">
+    </div>
+  </div>
+</div>
+
+
+
+
+
+<!--   ______________________________________________________-->
+
+
 </body>
+<?php require("partials/pager.php");?>
 </html>
