cdac3398532b81a5dcb4072fbf56b5aebf180e5f
Daniele Tommasini <daniele.tommasini.dt@gmail.com>
2018-04-12 10:42:24 +0000

    index/indexcontent

--- sitoweb-master/index.php
+++ sitoweb-master/index.php
@@ -5,64 +5,7 @@
 </div>
 <?php require("partials/toolbar.php");?>
 <body>
-  
-     <div class="container-fluid" style="background-color:#d34b41;color:#fff;height:200px;margin-left:1%;margin-right:1%;">
-  <h1>Problemi col tuo codice???</h1><br>
-<p>Non riesci a trovare il problema che non fa funzionare il tuo codice, application web o programma?</p><br>
-<h4>Condividilo cosicchè chiunque più o meno esperto di te può aiutarti a trovare il tuo errore e sistemare, migliorare , il tuo codice.</h4><br>
-</div>
-       <div class="container-fluid" style="background-color:#6265e8;color:#fff;height:200px;margin-left:1%;margin-right:1%;">
-  <h1 style="left:59%;position: relative;">Perchè condividere il tuo codice???</h1><br>
-<p>La condivisione non di basa solo sul cercare aiuto ma anche nella prevenzione.</p><br>
-<h4>Il tuo codice può essere sempre migliorato ma nel caso non potesse è una risorsa che potra servire a tutti.</h4><br>
-  </div>
-       <div class="container-fluid" style="background-color:#50bc54;color:#fff;height:200px;margin-left:1%;margin-right:1%;">
-  <h1>Perchè fare il lavoro degli altri???</h1><br>
-<p>Non ha senso fare nulla gratis ma come il 99% delle risorse open source la condivisione è fondamentale</p><br>
-<h4>Correggere e migliorare del codice che potra servire non solo all'utente che richiede aiuto ma anche a chiunque in futuro necessitare a infinite persone, pure a te.</h4><br>
-</div>
-       <div class="container-fluid" style="background-color:#a171b8;color:#fff;height:200px;margin-left:1%;margin-right:1%;">
-  <h1 style="left:59%;position: relative;">Per tutti???</h1><br>
-<p>Che sia un programma per l'universita, per la scuola o per lavoro perche non dovresti poter chiuedere aiuto? Imparando magari qualcosa?</p><br>
-<h4>Chi te lo vieta? Noi di certo no :)</h4><br>
-</div>
-<!--
-   <div class="col-sm-6" style="background-color:cornsilk; border-radius: 2%;z-index:-3;border-style:groove">
-    <h2>First example title </h2>
-      <p>Problem and description with data of push</p><br><br>
-      <p>
-coding to fix 
-  </p>
-  <!-- Left-aligned media object -->
- <!-- <div class="media">
-    <div class="media-left">
-     <span class='input-group-addon'style="width:60px"><i class='glyphicon glyphicon-envelope'></i></span>
-       
-    </div>
-    <div class="media-body">
-      <h4 class="media-heading">Left-aligned</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </div>
-  </div>
-  <hr>
-  
-  <!-- Right-aligned media object -->
-<!--  <div class="media">
-    <div class="media-body">
-      <h4 class="media-heading">Right-aligned</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </div>
-    <div class="media-right">
-      <img src="img_avatar1.png" class="media-object" style="width:60px">
-    </div>-->
-  </div>
-</div>
-
-
-
-
-
-
+<?php require("partials/content-index.php");?>
 </body>
 <?php require("/home/ubuntu/workspace/sitoweb/sitoweb-master/partials/pager.php");?>
 </html>
