fbbe4cabd49d970d89f9b3d853c5bb26a86e3554
Daniele Tommasini <daniele.tommasini.dt@gmail.com>
2018-04-17 07:41:25 +0000

    post all pages

--- sitoweb-master/partials/arduino.php
+++ sitoweb-master/partials/arduino.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <html>
 <?php require("toolbar.php");?>
-<body>
+
   <style>
-    body {
-    background-color: #000;
+body {
     color: #fff;
-    font-family: Verdana, sans-serif;
-    font-size: 14px;
 }
  
 #form {
@@ -24,32 +21,34 @@
  
 h1 {
     font-family: Verdana, sans-serif;
-    font-size: 16px;
-    color: #ff7070;
+    color: #FF8100;
     margin-bottom: 20px;
 }
 
-
+    
   </style>
-  
-   <form action="postinsert.php" id="f1" method="post">
-    <div id="form">
-
-    <h1>Scrivi qui per aggiungere il tuo post</h1>
-    <input style="display: none" type="text" name="category" value="arduino" />
-    <label for="title">title</label><input type="text" class="form-control" name="title" id="title" /><br>
-    <label for="desc">descfgn</label><input type="text" class="form-control" name="desc" id="desc" /><br>
-    <label for="content">Messaggio</label><br>
-    <textarea  name="textarea" id="msg" form="f1" class="form-control" cols="30" rows="10">write here ...</textarea>
- 
-    <input type="submit" id="submit" class="btn btn-default" name="submit" value="Invia"/>
-  </div>
-
+<body>
 <?php
 
 
+    session_start();
 
+    if(isset($_SESSION["id"])){
+        echo
+        "<form action='postinsert.php' id='f1' method='post'>
+            <div id='form'>
 
+            <h1>Type here to add your post</h1>
+            <input style='display: none' type='text' name='category' value='arduino' />
+            <label for='title'>Title</label><input type='text' class='form-control' name='title' id='title' /><br>
+            <label for='desc'>Description</label><input type='text' class='form-control' name='desc' id='desc' /><br>
+            <label for='content'>Message</label><br>
+            <textarea  name='textarea' id='msg' form='f1' class='form-control' cols='30' rows='10'>Type here ...</textarea>
+ 
+            <input type='submit' id='submit' class='btn btn-default' name='submit' value='Invia'/>
+            </div>
+        </form>";
+    }
 ?>
 
 </form>
--- sitoweb-master/partials/css.php
+++ sitoweb-master/partials/css.php
@@ -1,8 +1,52 @@
 <!DOCTYPE html>
 <html>
 <?php require("toolbar.php");?>
+<style>
+   body {
+    color: #fff;
+}
+ 
+#form {
+    background: #5f5f5f;
+    width:90%;
+    margin: 50px auto;
+    padding: 25px;
+    overflow: hidden;
+ 
+    -moz-border-radius: 20px;
+    -webkit-border-radius: 20px;
+    border-radius: 20px;
+}
+ 
+h1 {
+    font-family: Verdana, sans-serif;
+    color: #FF8100;
+    margin-bottom: 20px;
+}
+
+</style>
 <body>
-   css 
+    <?php
+
+
+    session_start();
+
+    if(isset($_SESSION["id"])){
+        echo
+        "<form action='postinsert.php' id='f1' method='post'>
+            <div id='form'>
+
+            <h1>Type here to add your post</h1>
+            <input style='display: none' type='text' name='category' value='css' />
+            <label for='title'>Title</label><input type='text' class='form-control' name='title' id='title' /><br>
+            <label for='desc'>Description</label><input type='text' class='form-control' name='desc' id='desc' /><br>
+            <label for='content'>Message</label><br>
+            <textarea  name='textarea' id='msg' form='f1' class='form-control' cols='30' rows='10'>Type here ...</textarea>
+ 
+            <input type='submit' id='submit' class='btn btn-default' name='submit' value='Invia'/>
+            </div>
+        </form>";
+    }
+?> 
 </body>
-<?php require("/home/ubuntu/workspace/sitoweb/sitoweb-master/partials/pager.php");?>
 </html>
