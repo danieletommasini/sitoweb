fff9638bcf83a13df4a2052318f220cf44267fad
Daniele Tommasini <daniele.tommasini.dt@gmail.com>
2018-05-08 07:55:24 +0000

    toolbar

--- sitoweb-master/account.php
+++ sitoweb-master/account.php
@@ -1,6 +1,53 @@
 <HTML>
     <?php require("partials/toolbar.php");?>
     <BODY>
+         <?php
+         session_start();
+         $id = $_SESSION["id"];
+        $servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "forum";
         
+        try {
+   				$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    			// set the PDO error mode to exception
+    			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $sql = $conn->prepare("SELECT users.email,users.username,users.password 
+                                            FROM `users` 
+                                            WHERE users.email 
+                                            like '$id'");
+				$sql->execute();
+				$result = $sql->fetchAll();
+                for($i=0; $i < count($result); $i++){
+                    echo '       
+                    <div class="panel panel-default" style="margin-left: 10px;margin-right: 10px;"> <h2 style="margin-left: 10px;margin-right: 10px;">Dati Personali</h2>
+                    <div class="panel-body" style="margin-left: 10px;margin-right: 10px;"><i style="color:#6ea4c5">Username </i>:'.$result[$i]["username"].'</div>
+                    <div class="panel-body" style="margin-left: 10px;margin-right: 10px;"><i style="color:#6ea4c5">email </i>:'.$result[$i]["email"].'</div>
+                    <div class="panel panel-default" style="margin-left: 10px;margin-right: 10px;"> <h3 style="margin-left: 10px;margin-right: 10px;">Change Password</h3>
+                    <div class="panel-body" style="margin-left: 10px;margin-right: 10px;"><i style="color:#6ea4c5">Old Password </i><input type="text" id="oldpass"></div>
+                    <div class="panel-body" style="margin-left: 10px;margin-right: 10px;"><i style="color:#6ea4c5">New Password </i><input type="text" id="newpass"></div>
+                    <div class="panel-body" style="margin-left: 10px;margin-right: 10px;"><i style="color:#6ea4c5">Reply Password </i><input type="text" id="newpassreply"></div>
+                    <buttom class="btn-info" onclick"VerifyPass()">Verify Password</button>
+                    ';
+                }
+        } catch(PDOException $e) {
+    		echo "Connection failed: " . $e->getMessage();
+        }
+    
+    ?>
+
+</div>
+<script>
+    function VerifyPass(){
+        if(password_verify($_POST["password"], $result[$i]["password"])){
+            
+            
+        }
+        
+        
+    }
+    
+</script>
     </BODY>
 </HTML>
--- sitoweb-master/partials/toolbar.php
+++ sitoweb-master/partials/toolbar.php
@@ -46,149 +46,116 @@ $(document).ready(function(){
   
 
 
-        
-<nav class="navbar navbar-inverse navbar-static-top " data-spy="affix" data-offset-top="197" style="z-index: unset;position:sticky">
+<nav class="navbar navbar-inverse navbar-static-top" data-spy="affix" data-offset-top="197" style="z-index: unset;position:sticky">
   <div class="container-fluid">
-<!--    <a class="w3-bar-item w3-button w3-hover-white w3-padding-16 hidesm" href="javascript:void(0)" onclick="w3_open()"><i class="fa">X</i></a>-->
     <div class="navbar-header">
-    <a class="navbar-brand" href="https://lamp-project-danieletommasini.c9users.io/sitoweb/sitoweb-master/"><p style="font-family:Impact, Charcoal, sans-serif">Scs</p></a>
-
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="https://lamp-project-danieletommasini.c9users.io/sitoweb/sitoweb-master/index.php"><p style="font-family:Impact, Charcoal, sans-serif">ScS</p></a>
     </div>
-    <ul id="content1" class="nav navbar-nav" >
-      <li><a href="https://lamp-project-danieletommasini.c9users.io/sitoweb/sitoweb-master/partials/arduino.php">Arduino</a></li>
-      <li><a href="https://lamp-project-danieletommasini.c9users.io/sitoweb/sitoweb-master/partials/cpp.php">C/C++</a></li>
-      <li><a href="https://lamp-project-danieletommasini.c9users.io/sitoweb/sitoweb-master/partials/java.php">Java</a></li>
-      <li><a href="https://lamp-project-danieletommasini.c9users.io/sitoweb/sitoweb-master/partials/html.php">Html</a></li>
-      <li><a href="https://lamp-project-danieletommasini.c9users.io/sitoweb/sitoweb-master/partials/css.php">Css</a></li>
-      <li><a href="https://lamp-project-danieletommasini.c9users.io/sitoweb/sitoweb-master/partials/javascript.php">JavaScript</a></li>
-      <li><a href="https://lamp-project-danieletommasini.c9users.io/sitoweb/sitoweb-master/partials/php.php">Php</a></li>
-         <form class="navbar-form navbar-left" action="/action_page.php">
-      <div class="form-group">
-        <input type="text" class="form-control" placeholder="Search" name="search">
-      </div>
-      <button type="submit" class="btn btn-default">Submit</button>
-    </form>
-     <!-- <li>  <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search ..">
-        <span class="input-group-btn">
-          <button class="btn btn-default" type="button">
-            <span class="glyphicon glyphicon-search"></span>
-          </button>
-        </span>
-      </div></li>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="/sitoweb/sitoweb-master/partials/arduino.php">Arduino</a></li>
+        <li><a href="/sitoweb/sitoweb-master/partials/cpp.php">C/C++</a></li>
+        <li><a href="/sitoweb/sitoweb-master/partials/java.php">Java</a></li>
+        <li><a href="/sitoweb/sitoweb-master/partials/html.php">Html</a></li>
+        <li><a href="/sitoweb/sitoweb-master/partials/css.php">Css</a></li>
+        <li><a href="/sitoweb/sitoweb-master/partials/javascript.php">JavaScript</a></li>
+        <li><a href="/sitoweb/sitoweb-master/partials/php.php">Php</a></li>
+        <form class="navbar-form navbar-left" action="/action_page.php">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Search" name="search">
+          </div>
+          <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <?php
+          
+            session_start();
+            if(!isset($_SESSION["id"])){
+              
+          echo "
+          <button type='button' class='btn btn-success btn-lg navbar-btn' style=' right: 8%; top: 1%;z-index:10; padding: 3px 12px;' data-toggle='modal' data-target='#myModal'><span class='glyphicon glyphicon-log-in'></span> Log In</button>
+          
+          <!-- Modal -->
+          <div id='myModal' class='modal fade' role='dialog'>
+            <div class='modal-dialog'>
+          
+              <!-- Modal content-->
+              <div class='modal-content'>
+                <div class='modal-header'>
+                  <button type='button' class='close' data-dismiss='modal'>&times;</button>
+                  <h4 class='modal-title'>Log In</h4>
+                </div>
+                <div class='modal-body'>";
+                
+             
+               include "/home/ubuntu/workspace/sitoweb/sitoweb-master/login.php";
+              
+              echo "
+              </div>
+                <div class='modal-footer'>
+                  <button type='button' class='btn btn-info' data-dismiss='modal'>Close</button>
+                </div>
+              </div>
+          
+            </div>
+          </div>
+          
+          <!-- ___________________________________________________-->
+          
+          <button type='button' class='btn btn-success btn-lg navbar-btn' style=' right: 1%; top: 1%;z-index:10; padding: 3px 12px;' data-toggle='modal' data-target='#signin'><span class='glyphicon glyphicon-user'></span> Sing In</button>
+          
+          <!-- Modal -->
+          <div id='signin' class='modal fade' role='dialog'>
+            <div class='modal-dialog'>
+          
+              <!-- Modal content-->
+              <div class='modal-content'>
+                <div class='modal-header'>
+                  <button type='button' class='close' data-dismiss='modal'>&times;</button>
+                  <h4 class='modal-title'>Sign In</h4>
+                </div>
+              <div class='modal-body'>";
+              
+              include"/home/ubuntu/workspace/sitoweb/sitoweb-master/signin.php";
+              
+              echo " 
+              </div>
+                <div class='modal-footer'>
+                  <button type='button' class='btn btn-info' data-dismiss='modal'>Close</button>
+                </div>
+              </div>
+          
+            </div>
+          </div>
+          
+          ";
+          } else {
+                echo "
+                <div class='navbar-right' style='margin-right: 0px'>  
+                  <ul class='nav navbar-nav'> 
+                   <li class='dropdown'>
+                     <a class='dropdown-toggle' data-toggle='dropdown' href='#'>" . $_SESSION["username"] . "<span class='caret'></span></a>
+                     <ul class='dropdown-menu'>
+                        <li><a href='/sitoweb/sitoweb-master/account.php'><div style='text-align:center'>Settings</div></a></li>
+                        <li><div style='text-align:center'><form class='form' action='/sitoweb/sitoweb-master/loginregister.php' method='post'>
+                            <input class='btn btn-primary' type='submit' name='logout' value='LOGOUT'> 
+                        </form></div></li>
+                     </ul>
+                    </li>
+                  </ul>
+                </div>";
+          }
 
-     <button type="button" class="btn btn-default btn-sm navbar-brand" style="background-color:#9d9d9d00" >
-          <span class="glyphicon glyphicon-list"></span> 
-        </button>
--->
-    </ul>
-
-    
-    
-    
-    
-<?php
-  session_start();
-  if(!isset($_SESSION["id"])){
-    
-echo "<div class='navbar-right' style='margin-right: 0px'>
-<button type='button' class='btn btn-success btn-lg navbar-btn' style=' right: 8%; top: 1%;z-index:10; padding: 3px 12px;' data-toggle='modal' data-target='#myModal'><span class='glyphicon glyphicon-log-in'></span> Log In</button>
-
-<!-- Modal -->
-<div id='myModal' class='modal fade' role='dialog'>
-  <div class='modal-dialog'>
-
-    <!-- Modal content-->
-    <div class='modal-content'>
-      <div class='modal-header'>
-        <button type='button' class='close' data-dismiss='modal'>&times;</button>
-        <h4 class='modal-title'>Log In</h4>
-      </div>
-      <div class='modal-body'>";
-      
-   
-     include "/home/ubuntu/workspace/sitoweb/sitoweb-master/login.php";
-    
-    echo "
-    </div>
-      <div class='modal-footer'>
-        <button type='button' class='btn btn-info' data-dismiss='modal'>Close</button>
-      </div>
-    </div>
-
-  </div>
-</div>
-
-<!-- ___________________________________________________-->
-
-<button type='button' class='btn btn-success btn-lg navbar-btn' style=' right: 1%; top: 1%;z-index:10; padding: 3px 12px;' data-toggle='modal' data-target='#signin'><span class='glyphicon glyphicon-user'></span> Sing In</button>
-
-<!-- Modal -->
-<div id='signin' class='modal fade' role='dialog'>
-  <div class='modal-dialog'>
-
-    <!-- Modal content-->
-    <div class='modal-content'>
-      <div class='modal-header'>
-        <button type='button' class='close' data-dismiss='modal'>&times;</button>
-        <h4 class='modal-title'>Sign In</h4>
-      </div>
-    <div class='modal-body'>";
-    
-    include"/home/ubuntu/workspace/sitoweb/sitoweb-master/signin.php";
-    
-    echo " 
-    </div>
-      <div class='modal-footer'>
-        <button type='button' class='btn btn-info' data-dismiss='modal'>Close</button>
-      </div>
-    </div>
-
-  </div>
-</div>
-
-</div>";
-} else {
-      echo "
-      <div class='navbar-right' style='margin-right: 0px'>  
-        <ul class='nav navbar-nav'> 
-         <li class='dropdown'>
-           <a class='dropdown-toggle' data-toggle='dropdown' href='#'>" . $_SESSION["username"] . "<span class='caret'></span></a>
-           <ul class='dropdown-menu'>
-              <li><a href='/sitoweb/sitoweb-master/account.php'><div style='text-align:center'>Settings</div></a></li>
-              <li><div style='text-align:center'><form class='form' action='/sitoweb/sitoweb-master/loginregister.php' method='post'>
-                  <input class='btn btn-primary' type='submit' name='logout' value='LOGOUT'> 
-              </form></div></li>
-           </ul>
-          </li>
-        </ul>
-      </div>";
-}
 ?>
-
-
-
-
-
-
-
-</div>
+      </ul>
+    </div>
+  </div>
 </nav>
-
-<!--
-<script>
-  if(screen.height>900){
-    getElementById("nav-bar1")
-  }
-  
-  
-</script>-->
-
-
-  <!--<form class='form' action='/sitoweb/sitoweb-master/loginregister.php' method='post'>
-      <input class='navbar-btn' type='submit' name='logout' value='LOGOUT'><br><br> 
-  </form>-->
-
-
-    </body>
+</body>
 </html>
